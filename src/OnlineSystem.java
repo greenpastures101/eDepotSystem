bb54cf54f50d708725dfaf331df01d8fbb86a9d0
Katlynn Chrans <k.chrans@2020ljmu.ac.uk>
2022-03-25 14:14:32 +0000

    Adding main method

--- src/OnlineSystem.java
+++ src/OnlineSystem.java
@@ -9,12 +9,36 @@ import java.util.Scanner;
 public class OnlineSystem {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+		new OnlineSystem().logInMenu();
 
 	}
-	// test comment
-	//test comment 2ggggfg
-	private Depot depot;
-	
 
+	//private final String PATH =
+	public final Scanner S = new Scanner(System.in);
+	private Driver driver;
+	
+	public void logInMenu() throws Exception {
+		String choice;
+		
+		do {
+			System.out.println("Please Log In to view Menu");
+			System.out.println("1 - Log In");
+			System.out.println("1 - Exit");
+			System.out.print("Enter option: ");
+			choice = S.nextLine();
+			
+			switch (choice) {
+				
+				case "1":
+					logIn();
+					break;
+			}
+		} while (!choice.equals("2"));
+		S.close();
+	}
+	
+	public void logIn() throws Exception {
+		
+	}
 
 }
