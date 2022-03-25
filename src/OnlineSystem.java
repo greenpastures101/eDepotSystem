2cb77d32b68d1cf76166e29a0a0a692572df6a95
Adam <adamalnajjar.dev@gmail.com>
2022-03-25 14:27:15 +0000

    Added ArrayList <Driver> and some code to try and read from file

--- src/OnlineSystem.java
+++ src/OnlineSystem.java
@@ -1,10 +1,13 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class OnlineSystem {
 	public static void main(String[] args) throws Exception {
@@ -13,7 +16,79 @@ public class OnlineSystem {
 
 	}
 
-	//private final String PATH =
+	
+	public static ArrayList<Driver> drivers = new ArrayList<>();
+	Scanner console = new Scanner(System.in);
+	
+	
+	
+//	private static void getDrivers() throws FileNotFoundException {
+//		 Scanner file = new Scanner(new File("/Users/adamal-najjar/Desktop/OO CW/csvfile_oo.csv"));
+//		 file.useDelimiter(",");
+	
+//	file = new BufferedReader(new FileReader("/Users/adamal-najjar/Desktop/OO CW/csvfile_oo.csv"))
+	
+	//importing csv file that needs to be parsed.
+	String fileToParse = "/Users/adamal-najjar/Desktop/OO CW/csvfile_oo.csv";
+	BufferedReader fileReader = null;
+	
+	//delimiter
+	final String DELIMITER = ",";
+	
+	try
+	{
+		String line = " ";
+		// Create the file reader
+		fileReader = new BufferedReader(new FileReader(fileToParse));
+		
+		//read the file line by line
+		 while ((line = fileReader.readLine()) != null) 
+		 {
+			 String[] tokens = line.split(DELIMITER);
+			 for (String token : tokens)
+			 {
+				 //print all tokens
+				 System.out.println(token);
+			 }
+			 
+		 }
+	}
+	catch (Exception e) {
+		e.printStackTrace();
+	}
+	finally
+    {
+        try {
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+	
+		
+		
+		
+//		while (file.hasNext()) {
+//			Driver driverData = new Driver(
+//					file.next(), 
+//					file.next(), 
+//					file.next(), 
+//					file.next(),
+//					file.nextBoolean(), 
+//					file.nextBoolean());
+//			drivers.add(driverData);
+//			
+//		}
+//		for (Driver driver : drivers) {
+//		 //System.out.println(drivers.toString());
+//		}
+//	
+//	}
+//	
+	
+	private final String PATH =
 	public final Scanner S = new Scanner(System.in);
 	private Driver driver;
 	
